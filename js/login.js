b5698e6c9657ccd9b7e7057e3c66ce47e0a8c1f4
beomshin.kim <chzh859@naver.com>
2023-12-12 21:10:00 +0900

    # 로그인 구현

---
+++ js/login.js
@@ -0,0 +1,8 @@
+function login(type) {
+    if (type == 'KKO') {
+        window.location.href = "http://formdang-api.com/api/sp/public/kakao/login";
+    } else if (type == 'GOOGLE') {
+        window.location.href = "http://formdang-api.com/api/sp/public/google/login";
+    }
+
+}
