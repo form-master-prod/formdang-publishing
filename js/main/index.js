759db6842639d32a324ed360b779a72d66ae55ee
leesungk <yskang@kusrc.co.kr>
2023-12-18 01:17:00 +0900

    로그인 임시

--- js/main/index.js
+++ js/main/index.js
@@ -20,31 +20,40 @@ $(window).load(() => {
 $(document).ready(function() {
     const token = localStorage.getItem('accessToken');
     if (token) {
-        $.ajax({
-            url: 'https://formdang-api.com/auth/sp/validate',
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            },
-            success: function(response) {
-                $(".user_login").remove();  // 로그인 화면 삭제
-                let html = `<div class="user_logout">
-                                <figure><img src="../image/common/not_profile.svg" alt="profile"></figure>
-                                <a href="javascript:void(0);" onclick="LOGOUT()">Logout <i class="ico"></i></a>
-                            </div>`;
-                $("#header").append(html);
-            },
-            error: function(xhr) {
-                if (xhr.status === 401) {
-                    console.log('401 Authentication failed');
-                    // alert("로그인을 다시 시도해주세요.");
-                    // window.location.href = "../admin/login.html"
-                } else {
-                    console.log('500 Internal logic failed');
-                    // alert("로그인을 다시 시도해주세요.");
-                    // window.location.href = "../admin/login.html"
-                }
-            }
-        });
+        $(".user_login").remove();  // 로그인 화면 삭제
+        let html = `<div class="user_logout">
+                        <figure><img src="../image/common/not_profile.svg" alt="profile"></figure>
+                        <a href="javascript:void(0);" onclick="LOGOUT()">Logout <i class="ico"></i></a>
+                    </div>`;
+        $("#header").append(html);
+
+        // $.ajax({
+        //     url: 'https://formdang-api.com/auth/sp/validate',
+        //     method: 'GET',
+        //     headers: {
+        //         'Authorization': `Bearer ${token}`
+        //     },
+        //     success: function(response) {
+        //         $(".user_login").remove();  // 로그인 화면 삭제
+        //         let html = `<div class="user_logout">
+        //                         <figure><img src="../image/common/not_profile.svg" alt="profile"></figure>
+        //                         <a href="javascript:void(0);" onclick="LOGOUT()">Logout <i class="ico"></i></a>
+        //                     </div>`;
+        //         $("#header").append(html);
+        //     },
+        //     error: function(xhr) {
+        //         if (xhr.status === 401) {
+        //             console.log('401 Authentication failed');
+        //             // alert("로그인을 다시 시도해주세요.");
+        //             // window.location.href = "../admin/login.html"
+        //         } else {
+        //             console.log('500 Internal logic failed');
+        //             // alert("로그인을 다시 시도해주세요.");
+        //             // window.location.href = "../admin/login.html"
+        //         }
+        //     }
+        // });
+        //
+
     }
 });
