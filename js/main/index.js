97f4efd2481dae8f612e61a27359e4044eae7cee
beomshin.kim <chzh859@naver.com>
2024-01-14 11:36:19 +0900

    # 메인 로그인 화면 처리

--- js/main/index.js
+++ js/main/index.js
@@ -3,7 +3,17 @@ function navigation(u) {
     else window.location.href = PAGE.LOGIN.MY;
 }
 
-function userHtml() {
+function userLogoutHtml() {
+    let html = ''
+    html =
+        html.concat(`<div class="user_login">`)
+            .concat(`<figure><img src="./image/common/not_profile.svg" alt="profile"></figure>`)
+            .concat(`<a href="./admin/login.html">Login <i class="ico"></i></a>`)
+            .concat(`</div>`)
+    return html;
+}
+
+function userLoginHtml() {
     let html = ''
     html =
         html.concat(`<div class="user_logout">`)
@@ -13,16 +23,33 @@ function userHtml() {
     return html
 }
 
-$(document).ready(() => {
-    const token = localStorage.getItem('accessToken');
+function appendUserLoginHtml() {
+    $(".user_login").remove();  // 로그인 화면 삭제
+    $("#header").append(userLoginHtml());
+}
+
+function appendUserLogoutHtml() {
+    $(".user_logout").remove();
+    $("#header").append(userLogoutHtml());
+}
+
+function loginIco() {
+    const token = localStorage.getItem(ACCESS_TOKEN);
     if (token) {
         uva2().then(r => {
             if (r && r.resultCode == '0') {
-                $(".user_login").remove();  // 로그인 화면 삭제
-                $("#header").append(userHtml());
+                appendUserLoginHtml()
+            } else {
+                appendUserLogoutHtml()
             }
-        })
+        }).catch(e => { appendUserLogoutHtml() })
+    } else {
+        appendUserLogoutHtml();
     }
+}
+
+$(document).ready(() => {
+    loginIco(); // 로그인 아이콘 처리
 })
 
 $(window).load(() => {
