c4971bb1207f149b6509a7580835715baf941d02
leesungk <yskang@kusrc.co.kr>
2024-02-11 01:47:15 +0900

    merge

--- js/admin/answer/form.js
+++ js/admin/answer/form.js
@@ -1,3 +1,21 @@
 function answerDetail(sid) {
-    console.log(sid)
+    const urlParams = new URLSearchParams(window.location.search);
+    const fidValue = urlParams.get('fid');
+
+    if(sid && fidValue) {
+        $.ajax({
+            type: 'POST',
+            url: 'https://formdang-api.com/api/dj/answers/detail',
+            data: {fid: fidValue, sid: sid},
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('accessToken')
+            },
+            success: function (response) {
+                console.log(response.answer_list);
+            },
+            error: function (error) {
+                console.error('AJAX 요청 실패:', error);
+            }
+        });
+    }
 }
